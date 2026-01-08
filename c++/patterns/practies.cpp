ffd769277e0238e16ba6e4fdc1195c34a9b31bf4
Md. Shah Alam <shahalam141072@gmail.com>
2026-01-08 10:43:55 +0600

    :up: => Working with pattern

--- c++/patterns/practies.cpp
+++ c++/patterns/practies.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
+    char ch = 'A';
     int count = 5;
     for (int i = 0; i < count; i++)
     {
@@ -13,8 +14,25 @@ int main()
 
         for (int k = 0; k < (count - i); k++)
         {
-            cout << i+1 << ' ';
+            cout << ch;
         }
+        ch++;
         cout << endl;
+    }
+
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = 1; j <= count; j++)
+        {
+            if (j >= (count + 1) - i)
+            {
+                cout << i;
+            }
+            else
+            {
+                cout << ' ';
+            }
+        }
+        cout << '\n';
     }
 }
