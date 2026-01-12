9e6ce5a9ffd91225d56cb41104b74805a445fdf8
Md. Shah Alam <shahalam141072@gmail.com>
2026-01-12 11:25:22 +0600

    :pencil2: => Print a series of number isPrime or not

--- c++/factorial/hw.cpp
+++ c++/factorial/hw.cpp
@@ -7,19 +7,30 @@ using namespace std;
 string isPrime(int number)
 {
     if (number == 1)
-        return "Not Prime";
+        return to_string(1) + " is Not Prime";
 
     for (int i = 2; i * i <= number; i++)
     {
         if (number % i == 0)
-            return "Not Prime";
+
+            return to_string(number) + " is Not Prime";
     }
-    return "Prime";
+    return to_string(number) + " is Prime";
 }
+
+void seriesOfPrimeNumber(int range)
+{
+    for (int i = 1; i <= range; i++)
+    {
+        cout << isPrime(i) << endl;
+    }
+}
+
 int main()
 {
     int number;
     cout << "Enter a number" << endl;
     cin >> number;
     cout << isPrime(number) << endl;
+    seriesOfPrimeNumber(number);
 }
