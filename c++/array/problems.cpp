f960396b6b26379aba1115aa5f22c3604487c7e0
Md. Shah Alam <shahalam141072@gmail.com>
2026-01-20 09:31:39 +0600

    :pencil2: => Using linear search find out target value (fixed the bug if target not found then return -1)

--- c++/array/problems.cpp
+++ c++/array/problems.cpp
@@ -56,21 +56,21 @@ void minMaxIndex(int numbers[], int size)
  * Find out a target value in an array by using linear search
  */
 
-void linearSearch(int numbers[], int size, int target)
+int linearSearch(int numbers[], int size, int target)
 {
     for (int i = 0; i < size; i++)
     {
         if (target == numbers[i])
         {
-            cout << "Target " << target << " found at index: " << i << endl;
-            return;
+            return i;
         }
     }
+    return -1;
 }
 int main()
 {
     int numbers[] = {10, 20, 30, 40, 50};
     minMax(numbers, 5);
     minMaxIndex(numbers, 5);
-    linearSearch(numbers, 5, 30);
+    cout << "Target found at index: " << linearSearch(numbers, 5, 100) << endl;
 }
