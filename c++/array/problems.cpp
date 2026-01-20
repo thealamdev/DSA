9ba453767e8d75fb5a6b86dd6a029caa4df62f81
Md. Shah Alam <shahalam141072@gmail.com>
2026-01-20 09:20:11 +0600

    :pencil2: => Solve min, max index problem

--- c++/array/problems.cpp
+++ c++/array/problems.cpp
@@ -16,14 +16,44 @@ void minMax(int numbers[], int size)
         {
             min = numbers[i];
         }
+
+        if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
     }
 
-    // cout << min << endl;
     cout << min << endl;
+    cout << max << endl;
 }
 
+/**
+ * Find out the max and min number index from an array
+ */
+
+void minMaxIndex(int numbers[], int size)
+{
+    int maxIndex = INT_MIN;
+    int minIndex = INT_MAX;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (numbers[i] > maxIndex)
+        {
+            maxIndex = i;
+        }
+
+        if (numbers[i] < minIndex)
+        {
+            minIndex = i;
+        }
+    }
+    cout << maxIndex << endl;
+    cout << minIndex << endl;
+}
 int main()
 {
     int numbers[] = {10, 20, 30, 40, 50};
     minMax(numbers, 5);
+    minMaxIndex(numbers, 5);
 }
